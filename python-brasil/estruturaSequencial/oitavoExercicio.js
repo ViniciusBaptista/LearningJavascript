9b1f6d472df7ad692220bc39a896343451d16833
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-14 01:05:55 -0300

    Oitavo Exercício

---
+++ python-brasil/estruturaSequencial/oitavoExercicio.js
@@ -0,0 +1,8 @@
+/**
+ * Faça um Programa que pergunte quanto você ganha por hora 
+ * e o número de horas trabalhadas no mês. 
+ * Calcule e mostre o total do seu salário no referido mês.
+ */
+function salario(horas, ganho) {
+  return Math.round(horas * ganho);
+}
