a64ae01a977b3e51f33d0ff95ec667c1d4905a8a
Vinicius Baptista <vbaptista7@gmail.com>
2014-07-14 09:49:17 -0300

    Corrigindo erros do Setimo Exercício

--- python-brasil/estruturaDecisao/setimoExercicio.js
+++ python-brasil/estruturaDecisao/setimoExercicio.js
@@ -1,7 +1,7 @@
 // Faça um Programa que leia três números e mostre o maior e o 
 // menor deles.
-function (n1, n2, n3) {
-  var x = [n1, n2, n3].sort();
+function leitura(n1, n2, n3) {
+  var numbers = [n1, n2, n3].sort();
 
-  console.log(x[0], x[2]);
+  console.log(numbers[0], numbers[2]);
 }
