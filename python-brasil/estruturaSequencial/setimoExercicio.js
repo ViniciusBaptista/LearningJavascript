c024916ba8dfa87ce25e6cf670da63c59bedf267
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-14 01:05:32 -0300

    Setimo Exercício

---
+++ python-brasil/estruturaSequencial/setimoExercicio.js
@@ -0,0 +1,6 @@
+// Faça um Programa que calcule a área de um quadrado, 
+// em seguida mostre o dobro desta área para o usuário.
+function area(l, a) {
+  // l => Lado, a => altura.
+  return (l * a) * 2;
+}
