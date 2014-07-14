6aba0c49214a5d831315c05a82d55d0d34ec3e61
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-14 01:06:14 -0300

    Nono Exercício

---
+++ python-brasil/estruturaSequencial/nonoExercicio.js
@@ -0,0 +1,5 @@
+// Faça um Programa que peça a temperatura em graus Farenheit,
+// transforme e mostre a temperatura em graus Celsius.
+function temperatura(f) {
+  return Math.round((f - 32) / 1.8);
+}
