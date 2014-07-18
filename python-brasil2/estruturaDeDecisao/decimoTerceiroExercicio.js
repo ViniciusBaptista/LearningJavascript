415df2904f6da9a8b6a234cec8def35320a4a5eb
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 22:42:55 -0300

    Decimo Terceiro Exercício

---
+++ python-brasil2/estruturaDeDecisao/decimoTerceiroExercicio.js
@@ -0,0 +1,13 @@
+// Faça um Programa que leia um número e exiba o dia correspondente
+// da semana.(1-Domingo, 2-Segunda, etc.), se digitar outro valor
+// deve aparecer valor inválido.
+function diasDaSemana(num) {
+  var semana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+      dia = num - 1;
+
+  if (num > 7) {
+    return 'Número Inválido';
+  }
+
+  return semana[dia];
+}
