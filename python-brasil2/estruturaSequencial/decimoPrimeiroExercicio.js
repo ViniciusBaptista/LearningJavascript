243d92ae6e6551c6516f94952e6d24fc01e884ea
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 21:04:30 -0300

    Decimo Primeiro Exercício

---
+++ python-brasil2/estruturaSequencial/decimoPrimeiroExercicio.js
@@ -0,0 +1,14 @@
+/**
+ * Faça um Programa que peça 2 números inteiro e um número real. Calcule e mostre:
+ *
+ * a. o produto do dobro do primeiro com metade do segundo.
+ * b. a soma do triplo do primeiro com o terceiro.
+ * c. o terceiro elevado ao cubo.
+ */
+function calculo(a, b, c) {
+  var produto = (a * 2) + (b / 2),
+      soma = (a * 3) + c,
+      cubo = Math.pow(c, 3);
+
+  return 'a. ' + produto + '\nb. ' + soma + '\nc. ' + cubo;
+}
