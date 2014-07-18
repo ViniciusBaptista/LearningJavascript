ce6ff6e6ea0588a4f9601626f98038c6840cbff0
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 21:04:54 -0300

    Decimo Segundo Exercício

---
+++ python-brasil2/estruturaSequencial/decimoSegundoExercicio.js
@@ -0,0 +1,5 @@
+// Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo
+// que calcule seu peso ideal, usando a seguinte fórmula: (72.7 * altura) - 58
+function pesoIdeal(altura) {
+  return 'Seu peso ideal é: ' + Math.round((72.7 * altura) - 58) + ' kg';
+}
