4455bc2728b0ec3d635b2bbf95799a5b3f37920f
Vinicius Baptista <vbaptista7@gmail.com>
2014-07-17 22:52:09 -0300

    Refatorando exercício

--- python-brasil2/estruturaDeDecisao/oitavoExercicio.js
+++ python-brasil2/estruturaDeDecisao/oitavoExercicio.js
@@ -1,7 +1,6 @@
 // Faça um programa que pergunte o preço de três produtos e informe qual
 // produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
 function comprar(valor1, valor2, valor3) {
-  var maisBarato = [valor1, valor2, valor3].sort();
 
-  return 'Produto mais barato custa: R$ ' + maisBarato[0];
+  return Math.min(valor1, valor2, valor3);
 }
