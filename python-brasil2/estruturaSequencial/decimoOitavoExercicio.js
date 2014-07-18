1149c0b75cf8d50a78ea37464951360fe3012be9
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 21:07:31 -0300

    Decimo Oitavo Exercício

---
+++ python-brasil2/estruturaSequencial/decimoOitavoExercicio.js
@@ -0,0 +1,14 @@
+/**
+ * Faça um programa que peça o tamanho de um arquivo para download (em MB)
+ * e a velocidade de um link de Internet (em Mbps), calcule e informe o
+ * tempo aproximado de download do arquivo usando este link (em minutos).
+ */
+function download(arquivo, velocidade) {
+  var tempo = arquivo / (velocidade * 60);
+  
+  if (tempo < 1) {
+    return 'Download sera concluido em menos de 1 minuto'
+  } else {
+    return 'Tempo de download: ' + Math.round(tempo) + ' minutos';
+  }
+}
