31b5739924e51f3c2f3d6e8a936ffc8cb6278336
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 22:38:37 -0300

    Segundo Exercício

---
+++ python-brasil2/estruturaDeDecisao/segundoExercicio.js
@@ -0,0 +1,5 @@
+// Faça um Programa que peça um valor e mostre na tela se o valor é
+// positivo ou negativo.
+function positivoOuNegativo(number) {
+  return number >= 0 ? 'Positivo' : 'Negativo';
+}
