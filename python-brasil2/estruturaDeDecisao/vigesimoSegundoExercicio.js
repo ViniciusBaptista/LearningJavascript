5854e4e52a742045999afb093787888056fe80c8
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 22:46:24 -0300

    Vigesimo Segundo Exercício

---
+++ python-brasil2/estruturaDeDecisao/vigesimoSegundoExercicio.js
@@ -0,0 +1,5 @@
+// Faça um Programa que peça um número inteiro e determine se ele é 
+// par ou impar. Dica: utilize o operador módulo (resto da divisão).
+function verificacao(numero) {
+  return numero % 2 === 0 ? 'Par' : 'Impar';
+}
