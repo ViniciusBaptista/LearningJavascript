ab9de6e582c9e5d2c8cf62fe530946891ab73c85
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 22:39:26 -0300

    Quarto Exercício

---
+++ python-brasil2/estruturaDeDecisao/quartoExercicio.js
@@ -0,0 +1,10 @@
+// Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+function verificacao(letra) {
+  var lower = letra.toLowerCase();
+
+  if ('aeiou'.indexOf(lower) >= 0) {
+    return 'Vogal';
+  } else {
+    return 'Consoante';
+  }
+}
