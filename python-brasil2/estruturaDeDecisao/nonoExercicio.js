a56c49ef0da6c94f303158e045b20060ca69ee8c
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 22:41:04 -0300

    Nono Exercício

---
+++ python-brasil2/estruturaDeDecisao/nonoExercicio.js
@@ -0,0 +1,5 @@
+// Faça um Programa que leia três número e mostre-os em ordem decrescente.
+function ordemDecrescente(numero1, numero2, numero3) {
+  var decrescente = [numero1, numero2, n3].sort();
+  return decrescente.reverse();
+}
