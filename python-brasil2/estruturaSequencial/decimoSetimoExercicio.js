fa50f35db28348661c2f45d6e715efbd83edd848
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 21:07:02 -0300

    Decimo Setimo Exercício

---
+++ python-brasil2/estruturaSequencial/decimoSetimoExercicio.js
@@ -0,0 +1,25 @@
+/**
+ * Faça um Programa para uma loja de tintas. O programa deverá pedir o 
+ * tamanho em metros quadrados da área a ser pintada. Considere que a 
+ * cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a 
+ * tinta é vendida em latas de 18 litros, que custam R$ 80,00 ou em 
+ * galões de 3,6 litros, que custam R$ 25,00.
+ *
+ * Informe ao usuário as quantidades de tinta a serem compradas e os 
+ * respectivos preços em 3 situações:
+ * comprar apenas latas de 18 litros;
+ * comprar apenas galões de 3,6 litros;
+ * misturar latas e galões, de forma que o preço seja o menor. 
+ * Acrescente 10% de folga e sempre arredonde os valores para cima, 
+ * isto é, considere latas cheias.
+ */
+function loja(area) {
+  var litros = area / 6,
+      qtsLatas = Math.round(litros / 18),
+      totalLata = Math.round(qtsLatas * 80),
+      qtsGalao = Math.round(litros / 3.6),
+      totalGalao = Math.round(qtsGalao * 25);
+
+  return 'Quantidade de Latas necessaria: ' + qtsLatas + '\n Valor total de latas: R$ ' + totalLata + 
+    '\n Quantidade de Galão necessaria: ' + qtsGalao + '\n Valor total de galões: R$ ' + totalGalao;     
+}
