d56d510ecc468046e951e2bd1b1edf2e0759e5a2
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 21:03:27 -0300

    Oitavo Exercício

---
+++ python-brasil2/estruturaSequencial/oitavoExercicio.js
@@ -0,0 +1,8 @@
+/**
+ * Faça um Programa que pergunte quanto você ganha por hora e o número de
+ * horas trabalhadas no mês. Calcule e mostre o total do seu sálario no
+ * referido mês.
+ */
+function salario(horasTrabalhadas, ganhoHora) {
+  return 'Seu sálario equivale a: R$ ' + (horasTrabalhadas * ganhoHora);
+}
