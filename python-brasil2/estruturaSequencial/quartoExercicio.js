9114f1b62468ac074fc1d6cb76ceb98a8072755d
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-17 21:01:47 -0300

    QuartoExercício

---
+++ python-brasil2/estruturaSequencial/quartoExercicio.js
@@ -0,0 +1,4 @@
+// Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+function bimestre(nota1, nota2, nota3, nota4) {
+  return 'A média é: ' + (nota1 + nota2 + nota3 + nota4) / 4;
+}
