961d80d1525d795400c3784c4cdf69b8f90bf8b2
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-19 18:33:46 -0300

    Quarto Exercício

---
+++ python-brasil2/exerciciosListas/quartoExercicio.js
@@ -0,0 +1,13 @@
+// Faça um Programa que leia um vetor de 10 caracteres, e diga quantas
+// consoantes foram lidas. Imprima as consoantes.
+function caracteres() {
+  var vogais = [],
+      consoantes = [];
+
+  for (var i = 0; i < 10; i++) {
+    'aeiou'.indexOf(arguments[i]) >= 0 ? vogais.push(arguments[i]) : consoantes.push(arguments[i]);
+  }
+
+  return 'Você digitou: ' + consoantes.length + ' consoantes' +
+         '\nConsoantes: ' + consoantes;
+}
