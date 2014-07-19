06a877c8bc3ebee99cd99f9fcf9600a67b0363dd
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-19 18:32:59 -0300

    Segundo Exercício

---
+++ python-brasil2/exerciciosListas/segundoExercicio.js
@@ -0,0 +1,11 @@
+// Faça um Programa que leia um vetor de 10 números reais e mostre-os
+// na ordem inversa.
+function vetor() {
+  var reais = [];
+
+  for (var i = 0; i < 10; i++) {
+    reais.push(arguments[i]);
+  }
+
+  return reais.reverse().join(', ');
+}
