3f60abfc0ce72fa84632bbce1c4b4b653be9ca6d
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-19 18:33:23 -0300

    Terceiro Exercício

---
+++ python-brasil2/exerciciosListas/terceiroExercicio.js
@@ -0,0 +1,7 @@
+// Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
+function notas(nota1, nota2, nota3, nota4) {
+  var notas = [nota1, nota2, nota3, nota4],
+      media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+  return 'Suas notas: ' + notas.join(', ') + '\nSua média: ' + Math.round(media);
+}
