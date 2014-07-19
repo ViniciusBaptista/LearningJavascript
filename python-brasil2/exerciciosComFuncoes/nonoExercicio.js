4ff81189083b2965b1011dbf533f5b87b94d8061
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-19 12:20:46 -0300

    Nono Exercício

---
+++ python-brasil2/exerciciosComFuncoes/nonoExercicio.js
@@ -0,0 +1,5 @@
+// Reverso do número. Faça uma função que retorne o reverso de um número
+// inteiro informado. Por exemplo: 127 -> 721.
+function reverso(number) {
+  return number.toString().split('').reverse().join('');
+}
