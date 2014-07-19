f472af9540eff5fbbcbbe84fb63b9a4b31fbd552
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-19 12:19:39 -0300

    Quarto Exercício

---
+++ python-brasil2/exerciciosComFuncoes/quartoExercicio.js
@@ -0,0 +1,8 @@
+/**
+ * Faça um programa, com uma função que necessite de um argumento.
+ * A função retorn o valor de caractere 'P', se seu argumento for
+ * positivo, e 'N', se seu argumento for zero ou negativo.
+ */
+function number(n) {
+  return n > 0 ? 'P' : 'N';
+}
