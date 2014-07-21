91efcc4d6f57882b4fcd7f6bf951b409115849e5
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-21 20:08:25 -0300

    Atualizando Decimo Nono Exercício

--- python-brasil2/estruturaDeDecisao/decimoNonoExercicio.js
+++ python-brasil2/estruturaDeDecisao/decimoNonoExercicio.js
@@ -1,14 +1,21 @@
-// Faça um Programa que leia um número inteiro menor que 1000 e 
-// imprima a quantidade de centenas, dezenas e unidades do mesmo.
+/**
+ * Faça um Programa que leia um número inteiro menor que 1000 e 
+ * imprima a quantidade de centenas, dezenas e unidades do mesmo.
+ */
+ 
 function numero(inteiro) {
-  
-  var str = numero.toString().split('');
-
-  if (str.length === 3) {
-    return str[0] + ' centenas, ' + str[1] + ' dezenas e ' + str[2] + ' unidades';
-  } else if (str.length === 2) {
-    return str[0] + ' dezenas e ' + str[1] + ' unidades'
-  } else if (str.length === 1) {
-    return str[0] + ' unidades';
+  var valor = inteiro
+              .toString()
+              .split('');
+ 
+  switch (valor.length) {
+    case 3:
+      return valor[0] + ' centenas, ' + valor[1] + ' dezenas e ' + valor[2] + ' unidades';
+    case 2:
+      return valor[0] + ' dezenas e ' + valor[1] + ' unidades';
+    case 1:
+      return valor[0] + ' unidades';
+    default:
+      return 'Valor inválido';
   }
 }
