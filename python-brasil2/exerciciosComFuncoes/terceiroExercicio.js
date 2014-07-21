f7c391256f45125d3abec209de199a0cf47b77ea
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-21 17:55:58 -0300

    Atualizando Terceiro Exercício

--- python-brasil2/exerciciosComFuncoes/terceiroExercicio.js
+++ python-brasil2/exerciciosComFuncoes/terceiroExercicio.js
@@ -1,5 +1,6 @@
 // Faça um programa, om uma função que necessite de três argumentos,
 // e que forneça a soma desses três argumentos.
-function soma(a,b,c) {
+
+function soma(a, b, c) {
   return a + b + c;
 }
