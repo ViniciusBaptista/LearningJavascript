3efc04cc89e06429be4ccb307d39031b5a9211b0
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-21 17:53:45 -0300

    Atualizando Oitavo Exercício

--- python-brasil2/exerciciosComFuncoes/oitavoExercicio.js
+++ python-brasil2/exerciciosComFuncoes/oitavoExercicio.js
@@ -1,5 +1,8 @@
 // Faça uma função que informe a quantidade de dígitos de um determinado
 // número inteiro informado.
+
 function digitos(number) {
-  return number.toString().length;
+  return number
+          .toString()
+          .length;
 }
