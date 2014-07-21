642deb68b9f409a2d79e6dcb63f84d21fa116a0d
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-21 17:54:17 -0300

    Atualizando Primeiro Exercício

--- python-brasil2/exerciciosComFuncoes/primeiroExercicio.js
+++ python-brasil2/exerciciosComFuncoes/primeiroExercicio.js
@@ -9,15 +9,19 @@
  * para um n informado pelo usuário. Use uma função que receba um
  * valor n inteiro e imprima até a n-ésima linha.
  */
+
 function estrutura(n) {
   var escada = '';
+  
   for (var i = 0; i <= n; i++) {
     var numeros = [];
 
     for (var x = 0; x < i; x++) {
       numeros.push(i);
     }
+  
     escada += numeros.join(' ') + '\n';
   }
+  
   return escada;
 }
