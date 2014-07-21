241e975a698f1abf125162b2c790d8097e46dd9d
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-21 17:54:44 -0300

    Atualizando Quarto Exercício

--- python-brasil2/exerciciosComFuncoes/quartoExercicio.js
+++ python-brasil2/exerciciosComFuncoes/quartoExercicio.js
@@ -3,6 +3,7 @@
  * A função retorn o valor de caractere 'P', se seu argumento for
  * positivo, e 'N', se seu argumento for zero ou negativo.
  */
+
 function number(n) {
   return n > 0 ? 'P' : 'N';
 }
