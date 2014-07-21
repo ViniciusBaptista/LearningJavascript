b0dbc1e9a43660e13399804269bfb431aeaabeba
ViniciusBaptista <vbaptista7@gmail.com>
2014-07-21 17:55:23 -0300

    Atualizando Segundo Exercício

--- python-brasil2/exerciciosComFuncoes/segundoExercicio.js
+++ python-brasil2/exerciciosComFuncoes/segundoExercicio.js
@@ -9,12 +9,19 @@
  * para um n informado pelo usuário. Use uma função que receba um valor 
  * n inteiro imprima até a n-ésima linha.
  */
+
 function estrutura(n) {
-  for (var i = 1; i <= n; i++) {
+  var escada = '';
+
+  for (var i = 0; i <= n; i++) {
     var numeros = [];
+   
     for (var x = 1; x <= i; x++) {
       numeros.push(x);
     }
-    console.log(numeros.join(' '));
+   
+    escada += numeros.join(' ') + '\n';
   }
+
+  return escada
 }
