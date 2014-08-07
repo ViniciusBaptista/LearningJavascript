41e97cf49b45e4b98835303d05d513ceb06c51ed
ViniciusBaptista <vbaptista7@gmail.com>
2014-08-07 19:42:57 -0300

    Modificando Quarto Exercício

--- python-brasil2/exerciciosListas/quartoExercicio.js
+++ python-brasil2/exerciciosListas/quartoExercicio.js
@@ -1,5 +1,8 @@
-// Faça um Programa que leia um vetor de 10 caracteres, e diga quantas
-// consoantes foram lidas. Imprima as consoantes.
+/**
+ * Faça um Programa que leia um vetor de 10 caracteres, e diga quantas 
+ * consoantes foram lidas. Imprima as consoantes.
+ */
+ 
 function caracteres() {
   var vogais = [],
       consoantes = [];
