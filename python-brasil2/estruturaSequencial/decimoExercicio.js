04bf1153c43cdd0032bd5d5e60a5ad4854037893
ViniciusBaptista <vbaptista7@gmail.com>
2014-08-09 15:06:50 -0300

    Atualizando Decimo Exercício

--- python-brasil2/estruturaSequencial/decimoExercicio.js
+++ python-brasil2/estruturaSequencial/decimoExercicio.js
@@ -1,5 +1,8 @@
-// Faça um Programa que peça a temperatura em graus Celsius, tranforme e mostre
-// em graus Fahrenheit.
+/**
+ * Faça um Programa que peça a temperatura em graus Celsius, tranforme e mostre
+ * em graus Fahrenheit.
+ */
+ 
 function transformacao(c) {
   return Math.round(c * 1.8 + 32) + ' °F';
 }
