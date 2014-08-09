d7ba7fbd05bf2bacef87abb0a4a4e28610e165e4
ViniciusBaptista <vbaptista7@gmail.com>
2014-08-09 14:53:09 -0300

    Atualizando Segundo Exercicio

--- python-brasil2/estruturaSequencial/segundoExercicio.js
+++ python-brasil2/estruturaSequencial/segundoExercicio.js
@@ -1,5 +1,8 @@
-// Faça um Programa que peça um número e então mostre a mensagem O número
-// informado foi [número].
+/**
+ * Faça um Programa que peça um número e então mostre a mensagem O número
+ * informado foi [número].
+ */
+ 
 function message(numero) {
   return 'O número informado foi: ' + numero;
 }
