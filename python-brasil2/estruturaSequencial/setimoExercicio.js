366be08cdfa6bac2c6ab3d7643aa7e2bf396cb3b
ViniciusBaptista <vbaptista7@gmail.com>
2014-08-09 14:52:09 -0300

    Atualizando Setimo Exercício

--- python-brasil2/estruturaSequencial/setimoExercicio.js
+++ python-brasil2/estruturaSequencial/setimoExercicio.js
@@ -1,5 +1,8 @@
-// Faça um programa que calcule a área de um quadrado, em seguida mostre
-// o dobro desta área para o usuário.
+/**
+ * Faça um programa que calcule a área de um quadrado, em seguida mostre
+ * o dobro desta área para o usuário.
+ */
+
 function area(lado1, lado2) {
   return 'O dobro da área do quadrado equivale a: ' + (lado1 * lado2) * 2;
 }
