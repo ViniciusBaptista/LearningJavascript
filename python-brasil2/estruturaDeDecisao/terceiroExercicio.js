db7a83501df9464f5297fe80f47b2b8cb794f00e
ViniciusBaptista <vbaptista7@gmail.com>
2014-08-10 23:33:01 -0300

    Atualizando Terceiro Exercício

--- python-brasil2/estruturaDeDecisao/terceiroExercicio.js
+++ python-brasil2/estruturaDeDecisao/terceiroExercicio.js
@@ -1,13 +1,17 @@
-// Faça um Programa que verifique se uma letra digitada é 'F' ou 'M'. 
-// Conforme a letra escrever: F - Feminino, M - Masculino, Sexo inválido.
+/**
+ * Faça um Programa que verifique se uma letra digitada é 'F' ou 'M'. 
+ * Conforme a letra escrever: F - Feminino, M - Masculino, Sexo inválido.
+ */
+
 function verifique(sexo) {
   var lower = sexo.toLowerCase();
-
-  if (lower === 'm') {
-    return 'Masculino';
-  } else if (lower === 'f') {
-    return 'Feminino';
-  } else {
-    return 'Sexo inválido';
+  
+  switch (lower) {
+    case 'm':
+      return 'Masculino';
+    case 'f':
+      return 'Feminino';
+    default:
+      return 'Sexo inválido';
   }
 }
