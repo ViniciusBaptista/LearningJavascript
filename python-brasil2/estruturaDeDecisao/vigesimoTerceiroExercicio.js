13e9fdd923d0d6fb39e2c4a8e03840e3938ec644
ViniciusBaptista <vbaptista7@gmail.com>
2014-08-10 23:34:13 -0300

    Atulizando Vigesimo Terceiro Exercício

--- python-brasil2/estruturaDeDecisao/vigesimoTerceiroExercicio.js
+++ python-brasil2/estruturaDeDecisao/vigesimoTerceiroExercicio.js
@@ -1,5 +1,8 @@
-// Faça um Programa que peça um número e informe se o número é 
-// inteiro ou decimal.
+/**
+ * Faça um Programa que peça um número e informe se o número é 
+ * inteiro ou decimal.
+ */
+ 
 function verificacao(numero) {
   return numero % 1 === 0 ? 'Inteiro' : 'Decimal';
 }
