beda5be45287428baa59f0bf11ab20205e567874
ViniciusBaptista <vbaptista7@gmail.com>
2014-08-10 23:31:07 -0300

    Atualizando Oitavo Exercício

--- python-brasil2/estruturaDeDecisao/oitavoExercicio.js
+++ python-brasil2/estruturaDeDecisao/oitavoExercicio.js
@@ -1,5 +1,8 @@
-// Faça um programa que pergunte o preço de três produtos e informe qual
-// produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
+/**
+ * Faça um programa que pergunte o preço de três produtos e informe qual
+ * produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
+ */
+ 
 function comprar(valor1, valor2, valor3) {
 
   return Math.min(valor1, valor2, valor3);
