eb6d3ad2d4792fa9db1a5ad43eca1936315f2325
ViniciusBaptista <vbaptista7@gmail.com>
2014-08-10 23:32:31 -0300

    Atualizando Setimo Exercício

--- python-brasil2/estruturaDeDecisao/setimoExercicio.js
+++ python-brasil2/estruturaDeDecisao/setimoExercicio.js
@@ -1,6 +1,7 @@
 // Faça um Programa que leia três números e mostre o maior e o menor deles.
+
 function verifique(n1, n2, n3) {
   var numbers = [n1, n2, n3].sort();
 
-  console.log(numbers[0], numbers[2]);
+  return (numbers[0]+ ', ' + numbers[2]);
 }
